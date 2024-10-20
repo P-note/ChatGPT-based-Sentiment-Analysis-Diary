4ad7e3b5025224497a7f375f2f1e0436e5beecc8
P-note <syoon0425@gmail.com>
2024-10-21 01:01:53 +0900

    10/21 work done

---
+++ test/test_llama3.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn.metrics import f1_score
+import ollama
+
+def load_reviews(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = file.readlines()
+    
+    reviews = []
+    for line in data:
+        score, review = line.split('\t')
+        reviews.append((int(score), review.strip()))
+    return pd.DataFrame(reviews, columns=['score', 'review'])
+
+def analyze_sentiment(review):
+    response = ollama.chat(model='llama3', messages=[
+        {
+            "role": "system", 
+            "content": "You are a professional in sentiment analysis. Check the review data given, and find out whether the sentiment is positive or negative. you may answer with positive or negative. only use these two words. don't print anything else."},
+        {
+                "role": "user",
+                "content": f"This is a review: '{review}' Is the sentiment of this review positive or negative?",
+        }
+    ])
+    sentiment = response['message']['content']
+    print(sentiment)
+
+    return 1 if sentiment == "positive" or sentiment ==  "Positive" else 0  # 1: Positive, 0: Negative
+
+def calculate_f1_score(df):
+    y_true = df['score'].apply(lambda x: 1 if x > 3 else 0)  # 1: 긍정(4~5), 0: 부정(1~2)
+    y_pred = df['review'].apply(analyze_sentiment)
+    return f1_score(y_true, y_pred)
+
+
+if __name__ == '__main__':
+    file_path = 'naver_shopping.txt'
+
+    df = load_reviews(file_path)
+
+
+    f1 = calculate_f1_score(df)
+    print(f"F1-score: {f1}")
