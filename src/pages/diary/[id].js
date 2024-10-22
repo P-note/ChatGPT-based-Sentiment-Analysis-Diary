3ea6439072b59ee57cf9d77a1389e252f8ef721e
P-note <syoon0425@gmail.com>
2024-10-22 17:15:35 +0900

    fixed backbutton z-index

--- src/pages/diary/[id].js
+++ src/pages/diary/[id].js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { Button, Flex } from '@radix-ui/themes';
+import { Button, Flex, IconButton, Container, Box, Section } from '@radix-ui/themes';
 
 export default function DiaryDetail() {
   const router = useRouter();
@@ -78,12 +78,12 @@ export default function DiaryDetail() {
   }
 
   return (
+    <Container maxWidth="60rem">
     <Flex>
-    <div style={styles.container}>
-      <button style={styles.backButton} onClick={() => router.back()}>
-        <img style={styles.backimg} src="/backward.svg"/>
-      </button>
-
+    <Button variant="soft" size="3" radius="full" style={styles.backButton} onClick={() => router.back()}>
+        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 11.3417 7.04882 11.6583 6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645L6.14645 3.14645C6.34171 2.95118 6.65829 2.95118 6.85355 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+    </Button>
+    <Section style={styles.container}>
       <h1 style={styles.title}>{diary.title}</h1>
       <h2 style={styles.date}>{new Date(diary.date).toLocaleDateString()}</h2>
       <p style={styles.content}>
@@ -98,9 +98,6 @@ export default function DiaryDetail() {
       <Button variant="soft" size="3" onClick={handleAnalyzeSentiment} disabled={analyzing}>
         {analyzing ? '분석 중...' : '감정 분석'}
       </Button>
-      {/* <button style={styles.analyzeButton} onClick={handleAnalyzeSentiment} disabled={analyzing}>
-        {analyzing ? '분석 중...' : '감정 분석'}
-      </button> */}
       
       {/* 감정 분석 결과 */}
       {analysis && (
@@ -115,8 +112,9 @@ export default function DiaryDetail() {
       </p>
         </div>
       )}
-    </div>
+    </Section>
     </Flex>
+    </Container>
   );
 }
 
@@ -129,18 +127,11 @@ const styles = {
     position: 'relative',
   },
   backButton: {
-    position: 'absolute',
-    top: '20px',
-    left: '20px',
-    fontSize: '24px',
-    background: 'none',
-    border: 'none',
-    color: '#0070f3',
+    position: 'fixed',
+    top: '5rem',
+    left: '4rem',
     cursor: 'pointer',
-  },
-  backimg:{
-    width: '3rem',
-    height: '3rem',
+    zIndex: 1000,
   },
   title: {
     fontSize: '24px',
